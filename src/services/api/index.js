6d75da5d045102b28d12eb5baeaa0ee965a15ebe
diana mamedova <mamedova.diana23@gmail.com>
2021-10-15 02:50:00 +0300

    feat: Add api servise

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -1,3 +1,13 @@
-const api = {}
+async function api(url, method) {
+  try {
+    const response = await fetch(url, { method })
+
+    if (response.ok !== true) {
+      throw new Error()
+    } else return response.json()
+  } catch (error) {
+    console.error(error)
+  }
+}
 
 export default api
